defcb1711d9b1cfa4f6744350ee97cf66c7eff6d
PavelPospolit <pavel.pospolit@telekom.de>
2023-01-31 14:44:40 +0100

    verbindung mit raspi

--- app/src/functions/handleLogIn.js
+++ app/src/functions/handleLogIn.js
@@ -1,29 +1,39 @@
 
 export default function handleLogIn(email, password, navigate) {
+    // (async () => {
+    // try {
+    // await fetch('/users', {
+    // method: 'POST',
+    // headers: {
+    //   'Accept': 'application/json, text/plain, */*',
+    //   'Content-Type': 'application/json'
+    //},
+    //body: JSON.stringify({ email: email, password: password })
+    // })
+    //.then((res) => {
+    //  return res.json()
+    //})
+    // .then(data => {
+    //if (!data.auth) {
+    // alert('Wrong Email and Password combination')
+    //}
+    //else {
+    //navigate('/')
+    //}
+    //})
+    // }
+    //catch (err) {
+    // console.log(err);
+    // }
+    //}) () 
     (async () => {
         try {
-            await fetch('/users', {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json, text/plain, */*',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ email: email, password: password })
-            })
-                .then((res) => {
-                    return res.json()
-                })
-                .then(data => {
-                    if (!data.auth) {
-                        alert('Wrong Email and Password combination')
-                    }
-                    else {
-                        navigate('/')
-                    }
-                })
+            await fetch('http://pavelpospolit.hopto.org/users')
+                .then((response) => response.json())
+                .then((data) => console.log(data));
         }
         catch (err) {
-            console.log(err);
+            console.log(err)
         }
     })()
 }
